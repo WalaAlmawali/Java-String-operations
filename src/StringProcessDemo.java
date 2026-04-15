2606b6fe9aec37d833f4488c49675fb86cdb0133
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-15 15:07:58 +0400

    use trim() method to remove extra space

--- src/StringProcessDemo.java
+++ src/StringProcessDemo.java
@@ -1,7 +1,10 @@
 public class StringProcessDemo {
     public static void main(String[] args){
         String record = "  Wala, IT, wala.salam.9888@gmail.com ";
-        System.out.println(" the original record is : " + record );
+        System.out.println(" the original record is : " +"\n" +record );
+
+        String trimedString = record.trim();
+        System.out.println(trimedString);
 
 
     }
