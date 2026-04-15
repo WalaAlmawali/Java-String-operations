ce785169f2bdf1158abbb0041841141c2c94b53e
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-15 15:05:30 +0400

    create string variable and desplay it on the screen

--- src/StringProcessDemo.java
+++ src/StringProcessDemo.java
@@ -1,5 +1,7 @@
 public class StringProcessDemo {
     public static void main(String[] args){
+        String record = "  Wala, IT, wala.salam.9888@gmail.com ";
+        System.out.println(" the original record is : " + record );
 
 
     }
