bf671474db43c36b8f0d370e07d99008ea80bda3
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-15 14:31:31 +0400

    Use the equals() method to compare 2 strings

--- src/StringCompareDemo.java
+++ src/StringCompareDemo.java
@@ -7,5 +7,10 @@ public class StringCompareDemo {
         System.out.println(word.charAt(2));
         System.out.println(word.substring(1,3));
 
+        String word2 = "mohammed";
+        System.out.println("is "+word + " equal to "+ word2 + "?" +word.equals(word2) );
+
+        String word3 = "Sara";
+        System.out.println("is "+word + " equal to "+ word3 + "?" +word.equals(word3));
     }
 }
