e324da3c2f14c51d7133966563a15379956e3a67
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-15 15:56:51 +0400

    Use the startsWith() method to check whether the first string starts with a given letter

--- src/StringAnalysisDemo.java
+++ src/StringAnalysisDemo.java
@@ -1,7 +1,7 @@
 public class StringAnalysisDemo {
     public static void main(String[] args){
 
-        String Text = " I love Java !";
+        String Text = "I love Java !";
         System.out.println(" the original Text is : " +"\n" +Text );
         System.out.println(" the total number of characters are : "+ Text.length());
 
@@ -10,6 +10,10 @@ public class StringAnalysisDemo {
         System.out.println("is "+Text + "equals to "+ Text2 + "?" + Text.equals(Text2) );
 
         System.out.println("the index of character 'v' in the string is :" + Text.indexOf("v") );
+
+
+        System.out.println("is the string start with 'I' character? " + Text.startsWith("I"));
+
 
     }
 }
