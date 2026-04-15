4e02873fb7b78558664851a0cdc59e311bb5801d
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-15 15:28:59 +0400

    create string variable and display it on the screen

--- src/StringCheckDemo.java
+++ src/StringCheckDemo.java
@@ -1,6 +1,8 @@
 public class StringCheckDemo {
     public static void main(String[] args){
 
+        String message = "welcome to our class";
+        System.out.println(" the original message is : " +"\n" +message );
 
     }
 }
