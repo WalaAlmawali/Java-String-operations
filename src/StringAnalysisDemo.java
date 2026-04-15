9eb3922908299fdc85634491cbed0528651d7039
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-15 15:47:43 +0400

    Create a string variable  with a sample Text and desplay it on the screen

--- src/StringAnalysisDemo.java
+++ src/StringAnalysisDemo.java
@@ -1,6 +1,8 @@
 public class StringAnalysisDemo {
     public static void main(String[] args){
 
+        String Text = " I love Java !";
+        System.out.println(" the original Text is : " +"\n" +Text );
 
     }
 }
