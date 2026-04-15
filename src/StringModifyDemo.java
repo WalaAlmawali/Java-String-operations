ddaa6cdd8223fc4a37aa877f60c752dd3aa7c340
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-15 14:52:14 +0400

    Use the replace() method to change one word in the string

--- src/StringModifyDemo.java
+++ src/StringModifyDemo.java
@@ -5,10 +5,14 @@ public class StringModifyDemo {
         String sentence = "  Hello   world  !";
 
         System.out.println(sentence);
+
         System.out.println(sentence.trim());
 
-
         System.out.println(" is the sentence contain world 'world' ? "+sentence.contains("world"));
+
+        String updatedSentence = sentence.replace("Hello", "Welcome");
+
+        System.out.println("sentenc after update : " + updatedSentence);
 
     }
 }
