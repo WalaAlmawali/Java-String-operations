fcdb27d585e6162ab206a843d3f5124ee4b5457d
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-15 16:09:19 +0400

    create a string variable 'sentence' and desplay it on the screen

--- src/StringDataDemo.java
+++ src/StringDataDemo.java
@@ -1,6 +1,9 @@
 public class StringDataDemo {
     public static void main(String[] args){
 
+        String sentence = "Oman is a big country";
+        System.out.println(" the original sentence is : " +"\n" +sentence );
+
 
     }
 }
