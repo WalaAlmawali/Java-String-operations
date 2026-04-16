663edca3c96424b7dfe25620712e3ef9dd501c5f
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 09:30:49 +0400

    Use the length() method to find the number of characters in each string

--- src/StringNestedLoopDemo.java
+++ src/StringNestedLoopDemo.java
@@ -6,6 +6,11 @@ public class StringNestedLoopDemo {
         System.out.println("First String: " + text1);
         System.out.println("Second String: " + text2);
 
+        System.out.println("Length of first String is: " + text1.length());
+        System.out.println("Length of second String is: " + text2.length());
+
+
+
 
     }
 }
