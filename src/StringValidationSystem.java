2b212cb043aec0d35a8984340f687f9437cf2efc
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 10:53:53 +0400

    create a Scanner object +Ask the user to enter a sample string  +Store the entered value in a String variable

--- src/StringValidationSystem.java
+++ src/StringValidationSystem.java
@@ -1,5 +1,12 @@
+
+import java.util.*;
+
 public class StringValidationSystem {
     public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a sample string: ");
+        String text = input.nextLine();
 
 
 
