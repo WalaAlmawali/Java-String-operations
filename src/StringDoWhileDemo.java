b00d5b5ec417dcc07116dfebc0442091b6d787ec
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 09:19:46 +0400

    create String variable and desplay it's value on the screen

--- src/StringDoWhileDemo.java
+++ src/StringDoWhileDemo.java
@@ -1,6 +1,7 @@
 public class StringDoWhileDemo {
     public static void main(String[] args){
-
+        String sentence = "Sara is a smart girl!";
+        System.out.println("Original String: " +"\n"+sentence );
 
     }
 }
