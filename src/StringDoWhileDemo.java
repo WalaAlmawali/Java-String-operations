f69a2d7a30ce53acb8c999a1921024e258c0c7bb
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 09:24:39 +0400

    Count the number of lowercase letters in the string

--- src/StringDoWhileDemo.java
+++ src/StringDoWhileDemo.java
@@ -7,10 +7,17 @@ public class StringDoWhileDemo {
 
 
         int i = 0;
+        int lowercaseLetters = 0;
+
 
         do {
             char ch = sentence.charAt(i);
             System.out.println(ch);
+
+            if (ch >= 'a' && ch <= 'z') {
+                lowercaseLetters++;
+            }
+
 
 
 
