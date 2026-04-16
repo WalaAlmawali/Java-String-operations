ae253063a794744539dd0f9e754ba951a1d17284
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 08:52:15 +0400

    create String variable and desplay it's value on the string

--- src/StringLoopDemo.java
+++ src/StringLoopDemo.java
@@ -1,6 +1,8 @@
 public class StringLoopDemo {
     public static void main(String[] args){
 
+        String sentence = "Java Programming";
+        System.out.println("Original String: " +"\n"+sentence );
 
     }
 }
