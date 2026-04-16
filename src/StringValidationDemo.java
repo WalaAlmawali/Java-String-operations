c1f471196dab6f48e0efe4552cc1db3414739f4f
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 08:21:04 +0400

    Use the endsWith() method to check whether the string ends with a specific word,

--- src/StringValidationDemo.java
+++ src/StringValidationDemo.java
@@ -6,6 +6,7 @@ public class StringValidationDemo {
         System.out.println("Original String: " +"\n"+text );
 
         System.out.println("Is the string start with 'It's'? " + text.startsWith("It's"));
+        System.out.println("Is the string ends with '.'? " + text.endsWith("."));
 
 
 
