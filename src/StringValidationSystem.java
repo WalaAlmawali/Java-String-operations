a45389a11b37bc883d82bbc9475493a583587848
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 10:57:08 +0400

    Use the toLowerCase() method to display the string in lowercase

--- src/StringValidationSystem.java
+++ src/StringValidationSystem.java
@@ -13,6 +13,8 @@ public class StringValidationSystem {
         System.out.println("Length of string: " + text.length());
 
         System.out.println("Uppercase: " + text.toUpperCase());
+        System.out.println("Lowercase: " + text.toLowerCase());
+
 
 
 
