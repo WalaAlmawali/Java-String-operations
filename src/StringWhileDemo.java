cccb02aac54aec1d7518623d09316c12cda55433
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 09:07:48 +0400

    create String variable and desplay it's value in the string

--- src/StringWhileDemo.java
+++ src/StringWhileDemo.java
@@ -1,6 +1,7 @@
 public class StringWhileDemo {
     public static void main(String[] args){
-
+        String sentence = "I love Programming";
+        System.out.println("Original String: " +"\n"+sentence );
 
     }
 }
