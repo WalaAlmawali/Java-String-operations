4d38d9237ab8366c8361a1026a9c41339078768c
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 10:11:48 +0400

    create String variable and desplay it's value on the string

--- src/StringIfElseDemo.java
+++ src/StringIfElseDemo.java
@@ -1,5 +1,7 @@
 public class StringIfElseDemo {
     public static void main(String[] args) {
+        String username = "Wala Al-mawali";
+        System.out.println("User name: " +"\n"+username );
 
 
 
