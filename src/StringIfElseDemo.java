2d9847d2ea61c1aed6abf748f0ac26dcadf838af
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 10:13:10 +0400

    Use an if statement to check if the username length is greater than 5

--- src/StringIfElseDemo.java
+++ src/StringIfElseDemo.java
@@ -3,7 +3,11 @@ public class StringIfElseDemo {
         String username = "Wala Al-mawali";
         System.out.println("User name: " +"\n"+username );
 
-
+        if (username.length() > 5) {
+            System.out.println("Username length is valid.");
+        } else {
+            System.out.println("Username is too short.");
+        }
 
     }
 }
