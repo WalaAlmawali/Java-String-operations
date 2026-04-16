c0d5da018452fc72870c88fd20cc8ccf414ae65f
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 09:29:40 +0400

    create two string variable and desplay them on the screen

--- src/StringNestedLoopDemo.java
+++ src/StringNestedLoopDemo.java
@@ -1,5 +1,10 @@
 public class StringNestedLoopDemo {
     public static void main(String[] args){
+        String text1 = "Hello";
+        String text2 = "World!";
+
+        System.out.println("First String: " + text1);
+        System.out.println("Second String: " + text2);
 
 
     }
