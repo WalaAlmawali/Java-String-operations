ea85873cf758886d057fe6d161f678d1722ee1c5
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 09:11:33 +0400

    create while loop and  Inside the loop, use the charAt() method to get each character

--- src/StringWhileDemo.java
+++ src/StringWhileDemo.java
@@ -4,7 +4,13 @@ public class StringWhileDemo {
         System.out.println("Original String: " +"\n"+sentence );
 
         System.out.println("Length of String is: " + sentence.length());
+        int i = 0;
 
+        while (i < sentence.length()) {
+
+            char ch = sentence.charAt(i);
+
+        }
 
     }
 }
