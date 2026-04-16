bff60d569dc5dc20f9833e65260d42d34c20d4ed
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 08:17:49 +0400

    create String variable and desplay it's value on the string

--- src/StringValidationDemo.java
+++ src/StringValidationDemo.java
@@ -2,6 +2,9 @@ public class StringValidationDemo {
     public static void main(String[] args){
 
 
+        String text = "It's my birthday!";
+        System.out.println("Original String: " +"\n"+text );
+
 
 
     }
