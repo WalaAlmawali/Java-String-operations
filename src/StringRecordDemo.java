a6f89add43e498d02f525a08742cddc2153d7ea2
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 08:01:09 +0400

    create String variable and desplay it on the screen

--- src/StringRecordDemo.java
+++ src/StringRecordDemo.java
@@ -2,6 +2,8 @@ public class StringRecordDemo {
     public static void main(String[]args){
 
 
+        String record = " Welcome to our country!  ";
+        System.out.println("Original String: " +"\n"+record );
 
     }
 }
