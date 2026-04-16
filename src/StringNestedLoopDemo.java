3d81062a63d9d9840f20345f8c0f35afeb564d93
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 09:36:17 +0400

    Inside the nested loops, use the charAt() method to compare characters from both

--- src/StringNestedLoopDemo.java
+++ src/StringNestedLoopDemo.java
@@ -9,10 +9,22 @@ public class StringNestedLoopDemo {
         System.out.println("Length of first String is: " + text1.length());
         System.out.println("Length of second String is: " + text2.length());
 
+        int matchCount = 0;
+
+
         for (int i = 0; i < text1.length(); i++) {
 
             for (int j = 0; j < text2.length(); j++) {
 
+                char ch1 = text1.charAt(i);
+                char ch2 = text2.charAt(j);
+
+                if (ch1 == ch2) {
+
+                    System.out.println("Matching character: " + ch1);
+                    matchCount++;
+
+                }
 
             }
 
