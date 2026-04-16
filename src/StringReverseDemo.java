dc1dc8e9bb685667e5baa8987b94c40dbd980798
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 09:48:24 +0400

    create String variable and desplay it's value on the string

--- src/StringReverseDemo.java
+++ src/StringReverseDemo.java
@@ -1,6 +1,9 @@
 public class StringReverseDemo {
     public static void main(String[] args) {
 
+        String text = "Java Programming";
+        System.out.println("Original String: " +"\n"+text );
+
 
 
     }
