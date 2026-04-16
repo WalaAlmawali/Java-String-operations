832c08d0c06f695073f187422cb54e23dfa56cec
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 10:36:35 +0400

    create String variable + Create a switch statement using the String variable

--- src/StringSwitchDemo.java
+++ src/StringSwitchDemo.java
@@ -1,6 +1,33 @@
 public class StringSwitchDemo {
     public static void main(String[] args) {
 
+        String role = "Admin";
+        System.out.println("User Role: " + role);
+
+        switch (role) {
+
+            case "Admin":
+
+                System.out.println("Access granted: Full system control.");
+                break;
+
+            case "User":
+                System.out.println("Access granted: Limited user access.");
+                break;
+
+            case "Guest":
+                System.out.println("Access granted: View only.");
+                break;
+
+            default:
+                System.out.println("Unknown role. Access denied.");
+
+
+
+
+
+
+        }
 
 
     }
