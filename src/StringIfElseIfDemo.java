e57ec65485273dbcc7bb8ff9ef063eb4e83bbddc
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-16 10:20:11 +0400

    create String variable and desplay it's value on the screen

--- src/StringIfElseIfDemo.java
+++ src/StringIfElseIfDemo.java
@@ -1,7 +1,8 @@
 public class StringIfElseIfDemo {
     public static void main(String[] args) {
 
-
+        String text = "Java Programming";
+        System.out.println("Original String: " +"\n"+text );
 
     }
 }
